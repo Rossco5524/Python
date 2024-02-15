0a42ec3022dca8491a59c3e7a8f10b69bd3050c6
Rossco5524 <127710456+Rossco5524@users.noreply.github.com>
2024-02-15 14:25:14 -0600

    Add files via upload

---
+++ Axel question game.py
@@ -0,0 +1,18 @@
+Dad = "Ross"
+Mom = "Alicia"
+siblings = "2"
+question_1 = input("Who's your Dad? : " "\n")
+question_2 = input("Who's your Mom? : " "\n")
+question_3 = input("How many siblings do you have? : " "\n")
+if Dad == question_1:
+    print("Q1 Correct!")
+else:
+    print("Q1 Incorrect!")
+if Mom == question_2:
+        print("Q2 Correct!")
+else:
+    print("Q2 Incorrect!")
+if siblings == question_3:
+    print("Q2 Correct!")
+else:
+    print("Q3 Incorrect!")
---
+++ ojibwe.py
@@ -0,0 +1,42 @@
+#Ojibwe Days of the Week Translator
+# By Ross A. Kleinmann
+# The symbol (-) in The Ojibwe has been replaced with (_) in order for program to run without error.
+# Please use (_) in place of (-)
+
+
+Ojibwe_word_list = ["Anama'e_giizhigad", "Nitam_anokii_giizhigad", "Niizho_giizhigad", "Aabitoose", "Niiyo_giizhigad",
+                    "Naano_giizhigad", "Giziibiigisaginige_giizhigad"]
+
+English_word_list = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+Day_of_Week = input("What day is it?" "\n")
+
+if Day_of_Week == English_word_list[0]:
+    print(Ojibwe_word_list[0])
+if Day_of_Week == English_word_list[1]:
+    print(Ojibwe_word_list[1])
+if Day_of_Week == English_word_list[2]:
+    print(Ojibwe_word_list[2])
+if Day_of_Week == English_word_list[3]:
+    print(Ojibwe_word_list[3])
+if Day_of_Week == English_word_list[4]:
+    print(Ojibwe_word_list[4])
+if Day_of_Week == English_word_list[5]:
+    print(Ojibwe_word_list[5])
+if Day_of_Week == English_word_list[6]:
+    print(Ojibwe_word_list[6])
+if Day_of_Week == Ojibwe_word_list[0]:
+    print(English_word_list[0])
+if Day_of_Week == Ojibwe_word_list[1]:
+    print(English_word_list[1])
+if Day_of_Week == Ojibwe_word_list[2]:
+    print(English_word_list[2])
+if Day_of_Week == Ojibwe_word_list[3]:
+    print(English_word_list[3])
+if Day_of_Week == Ojibwe_word_list[4]:
+    print(English_word_list[4])
+if Day_of_Week == Ojibwe_word_list[5]:
+    print(English_word_list[5])
+if Day_of_Week == Ojibwe_word_list[6]:
+    print(English_word_list[6])
+
